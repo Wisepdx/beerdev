1c09ee81af78269d1c21d2debefeca7d75ffe767
Jonathan Wise <Wisejon@users.noreply.github.com>
2016-04-28 14:59:28 -0700

    removed live server info

--- connect.php
+++ connect.php
@@ -5,12 +5,6 @@ $username = "root";
 $password = "root";
 $dbname = "beer";
 
-//Live Server
-// $servername="10.0.1.114";
-// $username="beer";
-// $password="wisepdx";
-// $dbname="beer";
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
