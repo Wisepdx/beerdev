bc75584e4734315d75583d32440778a74d4c8452
Jonathan Wise <Wisejon@users.noreply.github.com>
2016-04-28 14:58:58 -0700

    removed live server info

--- config.php
+++ config.php
@@ -1,11 +1,5 @@
 <?php
 
-    //Live Server
-    //define('DB_SERVER', '10.0.1.114');
-    //define('DB_USERNAME', 'beer');
-    //define('DB_PASSWORD', 'wisepdx');
-    //define('DB_DATABASE', 'beer');
-
     //TEST SERVER
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
